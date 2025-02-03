5cade592433367b26378fc82fd3f18173df45efa
EmreKb <emre.kb776@gmail.com>
2025-02-03 17:06:29 +0300

    feat: add gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/EmreKb/fiber-boilerplate/api"
 	"github.com/EmreKb/fiber-boilerplate/internal/config"
@@ -10,11 +13,14 @@ import (
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
 	config.Load()
-
 	database.NewPostgres(ctx, config.DatabaseUrl())
 
-	api.Bootstrap()
+	go api.Bootstrap()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+	<-signalCh
+	cancel()
 }
